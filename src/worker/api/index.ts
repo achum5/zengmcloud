9b26f84cce1784593fe13f6a95b132ab5572d512
Jeremy Scheff <jdscheff@gmail.com>
2026-09-12 09:27:10 -0400

    Cache.addAll
    (cherry picked from commit cd8d0e33fd05891ad1fab4cd7b8c8238136fe829)

--- src/worker/api/index.ts
+++ src/worker/api/index.ts
@@ -526,9 +526,7 @@ const allStarGameNow = async () => {
 	});
 
 	await idb.cache.schedule.clear();
-	for (const game of schedule) {
-		await idb.cache.schedule.add(game);
-	}
+	await idb.cache.schedule.addAll(schedule);
 
 	await initUILocalGames();
 	await updatePlayMenu();
@@ -6733,13 +6731,13 @@ const setScheduleFromEditor = async ({
 	}
 
 	await idb.cache.schedule.clear();
-
-	for (const game of schedule) {
-		if (game.type === "placeholder" || game.type === "completed") {
-			continue;
-		}
-		await idb.cache.schedule.add(omit(game, ["gid", "type"]));
-	}
+	await idb.cache.schedule.addAll(
+		schedule
+			.filter(
+				(game) => game.type !== "placeholder" && game.type !== "completed",
+			)
+			.map((game) => omit(game, ["gid", "type"])),
+	);
 
 	// This is needed in case the upcoming game was edited/deleted
 	await initUILocalGames();
