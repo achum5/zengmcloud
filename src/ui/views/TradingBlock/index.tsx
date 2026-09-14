c7a5367dc62b5a752e83529f65b4d9834e166b8e
Jeremy Scheff <jdscheff@gmail.com>
2026-09-13 22:10:44 -0400

    Clearer UI for untradeable players on trading block
    (cherry picked from commit 4eb0481b9ce4131e9696e8a0503767e3bf62fbf2)

--- src/ui/views/TradingBlock/index.tsx
+++ src/ui/views/TradingBlock/index.tsx
@@ -712,13 +712,15 @@ const TradingBlock = ({
 	);
 
 	const rows = userRoster.map((p) => {
+		const checked = state.pids.includes(p.pid);
+
 		return {
 			key: p.pid,
 			data: [
 				<input
 					className="form-check-input"
 					type="checkbox"
-					checked={state.pids.includes(p.pid)}
+					checked={checked}
 					disabled={p.untradable}
 					onChange={() => handleChangeAsset("pids", p.pid)}
 					title={p.untradableMsg}
@@ -741,6 +743,9 @@ const TradingBlock = ({
 				wrappedContractExp(p),
 				...stats.map((stat) => helpers.roundStat(p.stats[stat], stat)),
 			],
+			classNames: {
+				"table-danger": p.untradable && !checked,
+			},
 		};
 	});
 
