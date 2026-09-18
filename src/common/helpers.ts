ad84de15bb1ef0990df567522ac96ff5eeb36ac0
Jeremy Scheff <jdscheff@gmail.com>
2026-09-18 12:21:31 -0400

    More perf stuff
    (cherry picked from commit 906cee958d0d4625399616fc87288e205ba9116a)

--- src/common/helpers.ts
+++ src/common/helpers.ts
@@ -1058,12 +1058,22 @@ const localeParseFloat = (string: string) => {
 	);
 };
 
+const DEFAULT_MAXIMUM_FRACTION_DIGITS = 10;
+
+const numberWithCommasFormatter = new Intl.NumberFormat("en-US", {
+	maximumFractionDigits: DEFAULT_MAXIMUM_FRACTION_DIGITS,
+});
+
 // Format a number as an integer with commas in the thousands places.
 const numberWithCommas = (
 	x: number | string,
-	maximumFractionDigits: number = 10,
+	maximumFractionDigits: number = DEFAULT_MAXIMUM_FRACTION_DIGITS,
 ): string => {
 	const y = typeof x === "string" ? localeParseFloat(x) : x;
+
+	if (maximumFractionDigits === DEFAULT_MAXIMUM_FRACTION_DIGITS) {
+		return numberWithCommasFormatter.format(y);
+	}
 
 	return y.toLocaleString("en-US", { maximumFractionDigits });
 };
