20803941cabdc0854a27abb1509dfd13ff7666cf
Jeremy Scheff <jdscheff@gmail.com>
2026-09-18 11:09:54 -0400

    Lint
    (cherry picked from commit 118cdeb57429282340f305ee1621c46b7e3c264c)

--- src/worker/api/index.ts
+++ src/worker/api/index.ts
@@ -4123,7 +4123,7 @@ const regenerateSchedule = async (param: unknown, conditions: Conditions) => {
 		"noCopyCache",
 	);
 
-	const tids = await season.newSchedule(teams, conditions);
+	const tids = season.newSchedule(teams, conditions);
 
 	const schedule = season.addDaysToSchedule(
 		tids.map(([homeTid, awayTid]) => ({
@@ -5357,7 +5357,7 @@ const updateGameAttributesGodMode = async (
 	// Check schedule, unless it'd be too slow
 	const teams = (await idb.cache.teams.getAll()).filter((t) => !t.disabled);
 	if (teams.length < TOO_MANY_TEAMS_TOO_SLOW) {
-		await season.newSchedule(
+		season.newSchedule(
 			teams.map((t) => ({
 				tid: t.tid,
 				seasonAttrs: {
